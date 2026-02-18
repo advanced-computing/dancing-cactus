1b4c3e348ffb9e53f7f3e7479c162572a1707593
Eisuke Kobayashi <eisuke7296@gmail.com>
2026-02-17 21:07:45 -0500

    create a streamlit app

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+from datetime import date, timedelta
+
+ten_days_ago = date.today() - timedelta(days=10)
+selected_date = st.date_input(
+    label="select the date", max_value=date.today(), min_value=ten_days_ago
+)
+
+date = selected_date.strftime("%Y%m%d")
+
+# We use real-time market LBMP data
+data = pd.read_csv(f"https://mis.nyiso.com/public/csv/realtime/{date}realtime_zone.csv")
+
+# We focus on NYC for now.
+data = data[data["Name"] == "N.Y.C."]
+
+# convert columns to timestamps
+data["Time Stamp"] = pd.to_datetime(data["Time Stamp"], format="%m/%d/%Y %H:%M:%S")
+hourly_data = (
+    data.resample("H", on="Time Stamp")["LBMP ($/MWHr)"].mean().reset_index(name="LBMP")
+)
+st.line_chart(
+    data=hourly_data, x="Time Stamp", y="LBMP", x_label="Time", y_label="Price"
+)
