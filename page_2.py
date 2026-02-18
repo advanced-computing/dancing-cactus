3bb1c71809da534b0755d473c49ab416ea028ad7
JocelynJIANG-12 <cj2878@columbia.edu>
2026-02-18 13:56:23 -0500

    update page2 and req

--- page_2.py
+++ page_2.py
@@ -1,6 +1,35 @@
 import streamlit as st
+import pandas as pd
 
-st.markdown("# Page 2 ❄️")
-st.sidebar.markdown("# Page 2 ❄️")
+st.markdown("# Natural Gas Prices ❄️")
+st.sidebar.markdown("# Natural Gas Prices ❄️")
 
-st.write("Hello. This site is under construction")
+st.write("Henry Hub Natural Gas Spot Price (USD per Million Btu)")
+
+# Read Excel
+gas = pd.read_excel(
+    "data/RNGWHHDd.xls",
+    sheet_name="Data 1",
+    skiprows=3,  # Skip the first three row of Explanation
+)
+
+# Only keep first two col
+gas = gas.iloc[:, 0:2]
+
+# rename col
+gas.columns = ["Date", "Price"]
+
+gas = gas.dropna()
+
+# change date format
+gas["Date"] = pd.to_datetime(gas["Date"])
+
+gas = gas.sort_values("Date")
+
+st.line_chart(gas, x="Date", y="Price")
+
+
+st.markdown("""
+Natural gas prices are a key driver of electricity wholesale prices in NYC,
+since gas-fired plants often set the marginal price in the power market.
+""")
