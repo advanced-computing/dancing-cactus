14451b08e1eab97a845c2f6a6141931160788314
Eisuke Kobayashi <eisuke7296@gmail.com>
2026-02-24 22:04:34 -0500

    do unit testing

--- main_page.py
+++ main_page.py
@@ -21,25 +21,44 @@ selected_date = st.date_input(
 
 st.title(f"Daily Wholesale Electricity Prices in NYC:{selected_date}")
 
-month = selected_date.strftime("%Y%m01")
-date = selected_date.strftime("%Y%m%d")
 
-# We use real-time market LBMP data
-# find a proper zip file
-url = f"https://mis.nyiso.com/public/csv/realtime/{month}realtime_zone_csv.zip"
-response = requests.get(url)
+# generate monthly URL
+def get_zip(target_month):
+    month = target_month.strftime("%Y%m01")
+    zip_url = f"https://mis.nyiso.com/public/csv/realtime/{month}realtime_zone_csv.zip"
+    return zip_url
+
+
+# generate the name of target csv file
+def get_csv(target_date):
+    date_str = target_date.strftime("%Y%m%d")
+    csv = f"{date_str}realtime_zone.csv"
+    return csv
+
+
+response = requests.get(get_zip(selected_date))
 
 with zip.ZipFile(io.BytesIO(response.content), "r") as z:
-    with z.open(f"{date}realtime_zone.csv") as f:
+    with z.open(get_csv(selected_date)) as f:
         data = pd.read_csv(f)
 
-# We focus on NYC for now.
-data = data[data["Name"] == "N.Y.C."]
 
-# convert columns to timestamps
-data["Time Stamp"] = pd.to_datetime(data["Time Stamp"], format="%m/%d/%Y %H:%M:%S")
+# pick up one area and clean the data
+def process_nyiso_data(data, area):
+    filetered_data = data[data["Name"] == area]
+    filetered_data["Time Stamp"] = pd.to_datetime(
+        filetered_data["Time Stamp"], format="%m/%d/%Y %H:%M:%S"
+    )
+    return filetered_data
+
+
+# We focus on NYC for now.
+specific_data = process_nyiso_data(data, "N.Y.C.")
+
 hourly_data = (
-    data.resample("H", on="Time Stamp")["LBMP ($/MWHr)"].mean().reset_index(name="LBMP")
+    specific_data.resample("H", on="Time Stamp")["LBMP ($/MWHr)"]
+    .mean()
+    .reset_index(name="LBMP")
 )
 st.line_chart(
     data=hourly_data, x="Time Stamp", y="LBMP", x_label="Time", y_label="Price"
